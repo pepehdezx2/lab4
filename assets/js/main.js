63e37fb88cff3d3319fdb256c83849ece6662745
José Antonio Hernández <pepehdezx2@gmail.com>
2020-07-18 22:07:44 -0500

    Version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$('#add').click(function(event){
+	event.preventDefault();
+	var todoText = $('input').val();
+	if(todoText === ""){
+		alert("Sorry, you can't leave a blank space");
+	}
+	else{
+		$('ul').append('<li class="cc"> <p class="bold">' + todoText + '</p> <div> <button class="check"> check </button> <button class ="delete"> delete </button> </div> </li>');
+	}
+});
+
+$('div').on('click', '.delete', function(event){
+	event.preventDefault();
+	$(this).parent().parent().remove();
+});
+
+$('div').on('click', '.check', function(event){
+	event.preventDefault();
+	$(this).parent().parent().toggleClass("done");
+});
